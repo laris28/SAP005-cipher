98b1555d22f86188495d5ed8e74cdcec5c3adfe7
laris28 <larissalmeida2002@gmail.com>
2020-11-06 18:57:19 -0300

    subindo projeto Cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
 const cipher = {
-  // ...
+  encode(){
+    let fraseFinal = "";
+    const fraseCodificar = document.getElementById("codificar").value;
+    const deslocamento = parseInt(document.getElementById("deslocamento").value);
+    const numeroPrimeiraLetra = 65;
+    const tamanhoDoAlfabeto = 26;
+
+    for (let i=0; i<=fraseCodificar.length; i++) {
+      let resultado1 = (fraseCodificar.charCodeAt(i)+deslocamento-numeroPrimeiraLetra)%tamanhoDoAlfabeto;
+      let resultado2 = resultado1 + numeroPrimeiraLetra;
+      let imprimir = String.fromCharCode(resultado2);
+      fraseFinal += imprimir;
+    }
+    return fraseFinal;
+  },
+  decode(){
+    let fraseFinal = "";
+    const fraseDescodificar = document.getElementById("descodificar").value;
+    const deslocamento = parseInt(document.getElementById("deslocamento").value);
+    const numeroPrimeiraLetra = 65;
+    const tamanhoDoAlfabeto = 26;
+
+    for (let i=0; i<=fraseDescodificar.length; i++) {
+      let resultado1 = (fraseDescodificar.charCodeAt(i)-deslocamento+numeroPrimeiraLetra)%tamanhoDoAlfabeto;
+      let resultado2 = resultado1 + numeroPrimeiraLetra;
+      let imprimir = String.fromCharCode(resultado2);
+      fraseFinal += imprimir;
+    }
+    return fraseFinal;
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+document.getElementById("btn-codificar").addEventListener("click", () => btnSelecionado("encode"));
+document.getElementById("btn-descodificar").addEventListener("click", () => btnSelecionado("decode"));
+//() => arrow function utilizada no ES6
+
+function btnSelecionado(btnClicado) {
+  if (btnClicado === "encode") {
+    document.getElementById("codificado").value = cipher.encode();
+  } else {
+    document.getElementById("descodificado").value = cipher.decode();
+  }
+}
